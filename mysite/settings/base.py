20c5c78469ebedd688304316980c44fdafba6060
Misha Polishchyk <mishaelitzem2@rambler.ru>
2017-02-25 00:44:04 +0200

    commit102

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -162,6 +162,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
