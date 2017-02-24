561629ef481db89d3b9ab98d58d0c393fbec27f6
Misha Polishchyk <mishaelitzem2@rambler.ru>
2017-02-25 01:11:22 +0200

    commit105

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -285,9 +285,9 @@ IGNORABLE_404_URLS = (
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
