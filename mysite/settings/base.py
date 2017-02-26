2d0bc90746961d579ec95b96e434d77a4dfa4018
Misha Polishchyk <mishaelitzem2@rambler.ru>
2017-02-26 22:50:32 +0200

    add fixtures flatpages and auth

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -31,7 +31,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-SITE_ID = config('SITE_ID')
+SITE_ID = 1
 
 # SMTP backend(default) for send email
 
